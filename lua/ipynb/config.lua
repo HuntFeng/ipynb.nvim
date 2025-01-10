d55c008ba58872990749561d3ef819661e6f4b0b
huntf-bitstrata <hunt.feng@bitstrata.com>
2025-01-10 15:05:32 -0600

    able to update config

--- lua/ipynb/config.lua
+++ lua/ipynb/config.lua
@@ -1,13 +1,25 @@
-local options = {
+local default_options = {
 	image_scale_factor = 1.0,
 	save_outputs = false,
 }
 
-local function setup(opts)
-	options = vim.tbl_extend("force", options, opts)
+local opts = {}
+
+setmetatable(opts, {
+	__index = function(_, key)
+		return default_options[key]
+	end,
+	__newindex = function(_, key, value)
+		default_options[key] = value
+	end,
+})
+
+---@param options table
+local function setup(options)
+	default_options = vim.tbl_extend("force", default_options, options)
 end
 
 return {
-	opts = options,
+	opts = opts,
 	setup = setup,
 }
